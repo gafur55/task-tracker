92693fa564091bade72356ffd14302d6e3822b0d
gafur55 <g_mammadov@u.pacific.edu>
2022-09-17 19:20:42 -0700

    added Button, tasks components

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,32 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1, 
+        text: "task 1", 
+        day: 'Feb 5th at 1:30 pm',
+        reminder: true,
+    },
+    {
+        id: 2, 
+        text: "task 2", 
+        day: 'Feb 6th at 1:30 pm',
+        reminder: true,
+    },
+    {
+        id: 3, 
+        text: "task 3", 
+        day: 'Feb 7th at 1:30 pm',
+        reminder: false,
+    },
+])
   return (
     <div>
       <Header /> 
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,16 +1,22 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
+
 
 const Header = ({title}) => {
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
-    <Header className='header'>
+    <div className='header'>
         <h1>{title}</h1>
-        <button className='btn'>Add</button>
-    </Header>
+        <Button color='black' text='Add' onClick={onClick}/>       
+    </div>
   )
-}
+}   
 
 Header.defaultProps = {
-  title: 'Task Tracker'
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
